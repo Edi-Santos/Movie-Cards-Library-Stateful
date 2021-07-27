db82591419bf74ae6dbea62ec95c993af29b74d2
Ed Santos <edsantos2822@gmail.com>
2021-07-26 23:20:24 -0300

    Updates

---
+++ precache-manifest.13c552cda79d2e2f229040123eebd325.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b0b36b81733c3f742a4e1b4a1f22834",
+    "url": "/Movie-Cards-Library-Stateful/index.html"
+  },
+  {
+    "revision": "584934aed4ac0cf5643a",
+    "url": "/Movie-Cards-Library-Stateful/static/css/main.5d58c8a2.chunk.css"
+  },
+  {
+    "revision": "cbb6d5c7ef3b1ab528f2",
+    "url": "/Movie-Cards-Library-Stateful/static/js/2.9de1bedc.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/Movie-Cards-Library-Stateful/static/js/2.9de1bedc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "584934aed4ac0cf5643a",
+    "url": "/Movie-Cards-Library-Stateful/static/js/main.643bb932.chunk.js"
+  },
+  {
+    "revision": "4fcc8e694124b145109b",
+    "url": "/Movie-Cards-Library-Stateful/static/js/runtime-main.97514c80.js"
+  }
+]);
